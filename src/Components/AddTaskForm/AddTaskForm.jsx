c7dc62f0c5af742b5cf9bf8e79d4ff571366d58d
devRezaulKarim <dev.rezaulkarim@gmail.com>
2023-12-04 09:30:34 +0600

    input form done

---
+++ src/Components/AddTaskForm/AddTaskForm.jsx
@@ -0,0 +1,13 @@
+export default function AddTaskForm() {
+  return (
+    <div className="addTaskForm">
+      <form action="">
+        <input type="text" name="" id="" placeholder="Note your task" />
+        <div>
+          <span className="material-symbols-outlined">add_task</span>
+          <button>Add task</button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import AddTaskForm from "../Components/AddTaskForm/AddTaskForm";
+import TaskList from "../Components/TaskList/TaskList";
+import "./AllStyles.css";
+
+export default function HomePage() {
+  return (
+    <>
+      <div className="homeContainer">
+        <h1>
+          Task <span>Manage</span>ment
+        </h1>
+        <AddTaskForm />
+        <TaskList />
+      </div>
+    </>
+  );
+}
